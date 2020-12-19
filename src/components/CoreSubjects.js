ac3f8f623188a9d52ae005d60202d34a0229ecfb
DSC-Unimelb <pulkit@student.unimelb.edu.au>
2020-12-19 14:42:29 +0530

    Data.js changed

--- src/components/CoreSubjects.js
+++ src/components/CoreSubjects.js
@@ -25,11 +25,38 @@ export default function CoreSubjects(props) {
       items: [],
     },
   };
-  
-  return (
-    <div>
-      <h1>The Core subjects for this major is {columnsFromBackend.core.items}</h1>
-    </div>
-  );
+
+
+
+  const renderSubjects = () =>{
+    return(
+      props.inputText.map(( subject )=>{
+        return(
+          <div className='col-2 subs'>
+            <h2>{ subject[0] }</h2>
+            <h3>{ subject[1] }</h3>
+          </div>
+        )
+      })
+    )
+  }
+
+  if (props.inputText){
+    return(
+        <div>
+          <h1>The Core subjects for this major is {columnsFromBackend.core.items}</h1>
+          <div className='row sub-row'>
+            {renderSubjects()}
+          </div>
+        </div>
+      )  
+  }
+  else{
+    return(
+      <div>
+
+      </div>
+    )
+  }
 }
 
